0f8eb6f57fd7036437e83feb7959f10e32d5b889
Aten3D <atenlladojr@hotmail.com>
2024-10-16 10:52:00 +0200

    Cadenas 3

--- Ejercicios de Cadenas 2/Ej1.py
+++ Ejercicios de Cadenas 2/Ej1.py
@@ -1,4 +1,20 @@
+def buscainterfaz(interfaz,linea):
+    a=0
+    b=len(interfaz)
+    # while b <= len(linea):
+    #     if interfaz != linea[a:b]:
+    #         a+=1
+    #         b+=1
+    #     else:
+    #         return "Contiene la linea"
+    # Metodo facil
+    for interfaz in linea:
+        return "Contiene la linea"
+
 def main(args):
+    linea="iface eth0 inet static"
+    interfazabuscar="eth0"
+    print(buscainterfaz(interfazabuscar,linea))
     return 0
 
 if __name__ == '__main__':
--- Ejercicios de Cadenas 2/Ej2.py
+++ Ejercicios de Cadenas 2/Ej2.py
@@ -1,4 +1,7 @@
+def verificaip(ip):
+    return 0
 def main(args):
+    print(verificaip("192.168.1.1"))
     return 0
 
 if __name__ == '__main__':
