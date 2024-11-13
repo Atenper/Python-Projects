9923ec5099a9a578bbfbfdc5aeb22c64f1dcb805
Aten3D <atenlladojr@hotmail.com>
2024-11-13 01:54:47 +0100

    Listas 4

--- Listas/test2.py
+++ Listas/test2.py
@@ -1,7 +1,7 @@
 def main(args):
     lista = [('xilosofia', 5), ('zisica', 8), ('Mates', 4), ('Latin', 9), ('Historia', 8)]
     # Ordenar y filtrar en un solo paso
-    lista_ordenada = sorted([x for x in lista if x[1] >= 5], key=lambda x: x[0])
+    lista_ordenada = sorted([x for x in lista if x[1] >= 5], key=lambda x: x[1])
     
     print("Lista ordenada y filtrada:", lista_ordenada)
 
