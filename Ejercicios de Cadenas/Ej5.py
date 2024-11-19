048fa32e3d379c11b1878957dbb84f4e572cd101
Aten3D <atenlladojr@hotmail.com>
2024-11-19 22:46:36 +0100

    Repaso

--- Ejercicios de Cadenas/Ej5.py
+++ Ejercicios de Cadenas/Ej5.py
@@ -2,6 +2,8 @@ def main(args):
     frase=input("Introduce la fraccion: ")
     b=frase.split("/")
     print(b)
+    a= int(b[0])/int(b[1])
+    print("El resultado de la fraccion es",a)
     return 0
 
 if __name__ == '__main__':
