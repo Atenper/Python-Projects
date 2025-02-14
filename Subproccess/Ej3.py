4b2d0603b5de570b5bd79baa213be956873b8e3f
Aten3D <atenlladojr@hotmail.com>
2025-02-14 08:36:57 +0100

    Cambios

--- Subproccess/Ej3.py
+++ Subproccess/Ej3.py
@@ -1,12 +1,17 @@
 import os
 
+def enviro(variable):
+    print(os.environ[variable])
+    
 def info():
     print("Usuario actual:", os.getlogin())
     print("Variables de entorno:", os.environ)
     print("Sistema operativo:", os.name)
 
 def main(args):
-    info()
+    #info()
+    
+    enviro("SHELL")
     return 0
 
 if __name__ == '__main__':
